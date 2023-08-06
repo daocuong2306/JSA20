d93cffe90e58ff9278a9533442c2c1301dfa4f59
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-08-06 14:36:38 +0700

    B3 :login

---
+++ lesson3/login.js
@@ -0,0 +1,37 @@
+//B1 : tạo ra cơ sở dữ liệu
+const dataUser = [
+    {
+        id: 1,
+        name: 'Tèo',
+        acc: 'cuong123',
+        pass: 123
+    }
+];
+//B2 :Đẩy dữ liệu đã có lên local
+localStorage.setItem('dataUser', JSON.stringify(dataUser));
+//B3 : Lấy dữ liệu từ local
+const localUser = JSON.parse(localStorage.getItem('dataUser'));
+console.log(localUser);
+//B4 : tạo ra công việc đăng nhập
+    //b4.1 : lấy dự liệu từ 2 ô input bên html
+    //b4.2 : duyệt qua hết các phần tử trong dataUser
+    //b4.3 : So sánh và kiểm tra xem giá trị có trùng nhau không
+        //b4.3.1 : Nếu trùng thì thông báo thông tin đúng
+        //b4.3.2 : Nếu không trùng thì thông báo thông tin không đúng
+        //b4.3.3 : Thực hiện thao tác đăng nhập
+//B5 : tạo sự kiện đăng nhập
+//B6 : sau khi đăng nhập thành công thì cần lưu id của user 
+const Login = () => {
+    let acc = document.querySelector('.acc').value;
+    let pass = document.querySelector('.pass').value;
+    for (let user of localUser) {
+        if (acc == user.acc && pass == user.pass) {
+            localStorage.setItem("idUser", user.id)
+            alert('Đăng nhập thành công');
+            //dùng để chuyển trang
+            window.location.href = 'checkin.html';
+        } else {
+            alert('Đăng nhập thất bại');
+        }
+    }
+}
