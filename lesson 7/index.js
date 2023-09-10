013dc2fdc52b97d2a1c7aca0d5b766433ba7eeaf
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-09-10 15:35:18 +0700

    B 7 : API READ DELETE

---
+++ lesson 7/index.js
@@ -0,0 +1,35 @@
+
+
+
+const show = () => {
+    let app = document.querySelector('.item');
+    if (app) {
+        fetch('http://localhost:3000/product')
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                for (let item of data) {
+                    app.innerHTML += `
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>${item.name}</td>
+                        <td>${item.price}</td>
+                        <td><button onClick="deleteProduct(${item.id})">Xoa</button></td>
+                    </tr>
+            `
+                }
+            })
+    }
+
+}
+show()
+
+const deleteProduct = (id) => {
+    let check = window.confirm("bạn có muốn xóa");
+    if (check) {
+        fetch(`http://localhost:3000/product/${id}`, {
+            method: 'DELETE'
+        });
+        alert("đã xóa thành công")
+    }
+}
