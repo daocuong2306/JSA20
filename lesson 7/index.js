54615dff0644f983291b2cd83a922397cd6c77a0
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-09-17 14:36:51 +0700

    b8 : update

--- lesson 7/index.js
+++ lesson 7/index.js
@@ -42,3 +42,29 @@ const addProduct = () => {
 
     });
 }
+
+
+const showProductById = () => {
+    const id = new URLSearchParams(window.location.search).get('id');
+    if (id) {
+        let app = document.querySelector('.app');
+        fetch(`http://localhost:3000/product/${id}`)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                app.innerHTML = `
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                    <input type="text" class="form-control name" id="exampleInputEmail1" aria-describedby="emailHelp" value=${data.name}>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Price</label>
+                    <input type="number" class="form-control price" id="exampleInputPassword1" value=${data.price}>
+                </div>
+                <button type="submit" class="btn btn-primary" >Submit</button>
+
+                `
+            })
+    }
+}
+showProductById()
